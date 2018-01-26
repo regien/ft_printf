9d69159e4cfc428b28278acd8a9b0a77689fb440
regien <gerardomalpartidavela@gmail.com>
2018-01-26 13:17:54 -0800

    something not fully working

--- main.c
+++ main.c
@@ -6,8 +6,24 @@
 /*   By: gmalpart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 21:10:39 by gmalpart          #+#    #+#             */
-/*   Updated: 2018/01/24 21:10:56 by gmalpart         ###   ########.fr       */
+/*   Updated: 2018/01/26 13:17:30 by regien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
+
+int	ft_printf(char const *str, ...)
+{
+	int		i;
+	
+	va_start(args, str);
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == '%')
+			parser(&i, str);
+		else
+			ft_putchar(str[i]);
+	}
+	return (i);
+}
